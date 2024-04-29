288b89899d7fc10b18add06ef9bbb7b5b8e7c12c
Shubasish Das <shubasish@Shubasishs-MacBook-Pro.local>
2024-04-29 19:28:14 +0600

    update the configuration to google auth provider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { signIn, signOut, useSession } from "next-auth/react";
 import {
   Button,
   Layout,
@@ -7,17 +7,19 @@ import {
   Text,
   List,
   Code,
-} from '@vercel/examples-ui'
+} from "@vercel/examples-ui";
 
 export default function Home() {
-  const { data, status } = useSession()
+  const session = useSession();
+  const { data, status } = session;
+  console.log({ data, status, session });
 
   return (
     <Page>
       <section className="flex flex-col gap-6">
         <Text variant="h1">Securing routes using next-auth</Text>
         <Text>
-          Wrapping our <Code>pages/_app</Code> using{' '}
+          Wrapping our <Code>pages/_app</Code> using{" "}
           <Code>SessionProvider</Code> from <Code>next-auth</Code> will secure
           all our pages. If we configure sub domains or rewrites, all will be
           behind an auth wall.
@@ -27,37 +29,37 @@ export default function Home() {
       <hr className="border-t border-accents-2 my-6" />
 
       <section className="flex flex-col gap-3">
-        {status === 'authenticated' ? (
+        {status === "authenticated" ? (
           <section className="flex flex-col gap-3">
-            Welcome {data?.user?.name}!{' '}
+            Welcome {data?.user?.name}!{" "}
             <Button onClick={() => signOut()}>Sign out</Button>
             <List>
               <li>
-                <Link href="https://subdomain.solutions-subdomain-auth.vercel.sh">
+                <Link href="http://v1.retune.localhost:3005">
                   subdomain.solutions-subdomain-auth.vercel.sh
                 </Link>
               </li>
               <li>
-                <Link href="https://solutions-subdomain-auth.vercel.sh">
+                <Link href="http://retune.localhost:3005">
                   solutions-subdomain-auth.vercel.sh
                 </Link>
               </li>
             </List>
           </section>
-        ) : status === 'loading' ? (
+        ) : status === "loading" ? (
           <section className="text-center">
             <Text>Loading...</Text>
           </section>
         ) : (
           <section className="m-auto w-fit">
-            <Button size="lg" onClick={() => signIn('github')}>
-              Sign in with GitHub
+            <Button size="lg" onClick={() => signIn("google")}>
+              Sign in with Google
             </Button>
           </section>
         )}
       </section>
     </Page>
-  )
+  );
 }
 
-Home.Layout = Layout
+Home.Layout = Layout;
